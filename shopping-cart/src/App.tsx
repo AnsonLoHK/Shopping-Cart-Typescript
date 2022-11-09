da51f913b503b4362821eeffa30ab16cb9e9711a
LAPTOP-U9LNG27H\essences24775257 <chiaochiao1106@gmail.com>
2022-11-09 17:51:12 +0800

    初始

---
+++ shopping-cart/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+import ProducstList from "./Products/ProducstList";
+
+function App() {
+  return (
+    <div className="App">
+      <ProducstList />
+    </div>
+  );
+}
+
+export default App;
---
+++ shopping-cart/src/Products/ProducstList.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface ProducstListProps {
+  // : string;
+}
+
+const ProducstList: React.FC<ProducstListProps> = ({}) => {
+  return (
+    <div>
+      <label htmlFor="">game list</label>
+    </div>
+  );
+};
+
+export default ProducstList;
