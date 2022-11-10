b259a14f46122754e3d9f47363d39ce50c634d02
LAPTOP-U9LNG27H\essences24775257 <chiaochiao1106@gmail.com>
2022-11-10 12:08:47 +0800

    客製化按鈕 觸發增加product功能

--- shopping-cart/src/Products/ProducstList.tsx
+++ shopping-cart/src/Products/ProducstList.tsx
@@ -1,15 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 
-interface ProducstListProps {
-  // : string;
+interface ProducstListProps {}
+
+const initialProducst = [
+  {
+    title: "purple",
+    price: 50,
+    id: "eft",
+  },
+  {
+    title: "hunt purple",
+    price: 50,
+    id: "hunt",
+  },
+  {
+    title: "hll purple",
+    price: 50,
+    id: "hll",
+  },
+];
+
+interface Product {
+  title: string;
+  price: string;
+  id: string;
 }
 
 const ProducstList: React.FC<ProducstListProps> = ({}) => {
+  const [products, setProducts] = useState(initialProducst);
+
+  const AddProduct = () =>
+    setProducts((prevProducts) => [
+      {
+        title: "dsdd",
+        price: 50,
+        id: "efsdst",
+      },
+      ...prevProducts,
+    ]);
+
   return (
     <div>
-      <label htmlFor="">game list</label>
+      <h2>game list</h2>
+      {products.map((product) => (
+        <div key={product.id}>
+          <span>
+            {`${product.title}`} ${product.price}
+          </span>
+        </div>
+      ))}
+
+      <ButtonPrimary
+        oneOfYourProps
+        onClick={AddProduct}
+        children="Add Product"
+      />
     </div>
   );
 };
 
 export default ProducstList;
+
+// TODO: 定義 Button
+export interface ButtonProps
+  extends React.DetailedHTMLProps<
+      React.ButtonHTMLAttributes<HTMLButtonElement>,
+      HTMLButtonElement
+    >,
+    React.AriaAttributes {
+  oneOfYourProps: unknown;
+}
+
+export const ButtonPrimary: React.FC<ButtonProps> = (props) => {
+  const { children, ...rest } = props;
+
+  return <button {...rest}>{children}</button>;
+};
+
+// TODO!: 參考
+// https://stackoverflow.com/questions/60436233/creating-a-custom-button-in-react-typescript-and-add-onclick-event
